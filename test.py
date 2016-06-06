bdc21295ba5f7f3dfd7c485b19ad6088be8de3f6
root <rezkon93@gmail.com>
2016-06-07 02:24:54 +1000

    this is a just a test

---
+++ test.py
@@ -0,0 +1,17 @@
+import socket 
+import os 
+import codecs 
+
+
+
+print "This is a just a V0.1 Draft" 
+
+user = raw_input("Please enter a name: \n")
+
+
+print "This users name is %s" % user 
+
+exit()
+
+
+
