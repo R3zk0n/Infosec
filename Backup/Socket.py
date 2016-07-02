4534ec25176085f2d33b5aa0d9255b4061e8ff74
R3zk0n <rezkon93@gmail.com>
2016-07-02 04:21:17 -0400

    Adding Backup

---
+++ Backup/Socket.py
@@ -0,0 +1,50 @@
+#Basic Python Socket implementation, Banner Grabbing
+# Thus we sit as one man, confused lost and stressed, no job and no money but with alot of talent and a gift for being able to adapt to all things.. 
+
+
+# Added Updated Port Range..
+import socket
+import sys
+import subprocess
+import select
+#Clearing Screen.
+subprocess.call('clear', shell=True)
+
+print "Basic Python Sockets" 
+
+User_input = raw_input("Select.. 1 . Selective Scanner || 2 . Range Scanner \n")
+
+# Add Selective ports and port range scanner. 
+if User_input == '1':
+	IP = raw_input("Please enter a IP Address: \n")
+	port = input("Please Enter a port num to scan :\n")
+	socket.setdefaulttimeout(1)
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect_ex((IP,port))
+		s.send("Hello")
+		ans = s.recv(4096)
+		print IP
+		print "[+] The IP "+str(IP)+" On Port: "+str(ans)
+
+	except socket.error:
+		print "NO ACCESS."
+		sys.exit()
+	
+
+if User_input == '2':
+	Ip2 = raw_input("Please enter a ip address to scan \n")
+	socket.setdefaulttimeout(1)
+	try:
+		for port in range(1,1025):
+			print "Port {}: %s" % port
+			sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			connection = sck.connect_ex((Ip2, port))
+			if connection == 0:
+				sck.send("Hello")
+				ans = sck.recv(1024)
+				print ans
+				sck.close()
+	except socket.error:
+		print "No Connection"
+		sys.exit()
