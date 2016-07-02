623e5417aca307646fa4a72a75a8d91c3e996852
R3zk0n <rezkon93@gmail.com>
2016-07-02 04:51:50 -0400

    V.1

--- Hardware/Serial.py
+++
@@ -1,3 +0,0 @@
-import serial #import the serial library
-arduinoSerialPort = serial.Serial("ttyACM0", 9600)
-arduinoSerialPort.write('b')
--- Hardware/requirements.py
+++
@@ -1,3 +0,0 @@
-pyFirmata==0.9.5
-BreakfastSerial==0.0.9
-twilio==3.6.2
