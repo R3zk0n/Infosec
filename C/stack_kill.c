2d9eba8e5186d76033cff74a92753792e53c3df7
R3zk0n <rezkon93@gmail.com>
2016-07-15 11:09:51 -0400

    Banner scanner, and Stack stuff added

---
+++ C/stack_kill.c
@@ -0,0 +1,13 @@
+
+
+void concept_function(int a, int b, char c, char d)
+{
+  int flag;
+  char buffer[10];
+
+  flag = 1337;
+  buffer[0] = "A";
+}
+  int main(){
+    concept_function(1, 2, 3, 0X61);
+  }
