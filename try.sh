553d3ecfca342703de15fefd9b30d04c963e80d2
Luke Symons <rezkon93@gmail.com>
2016-09-03 02:28:16 +1000

    Add files via upload

---
+++ try.sh
@@ -0,0 +1,19 @@
+RED='\033[0;31m'
+YELLOW='\033[0;32m'
+
+echo "Backup.."
+echo "Web"
+git clone https://github.com/jhaddix/domain
+git clone https://github.com/jhaddix/tbhm
+git clone https://github.com/4ido10n/latest-Burpsuite-Pro-v1.7.03-cracked-version
+git clone https://github.com/infosec-au/bugbountydash
+git clone https://github.com/danielmiessler/SecLists
+echo -e "${YELLOW}Exploitation Stuff\n" 
+git clone https://github.com/pwndbg/pwndbg
+git clone https://github.com/SaltwaterC/sploit-tools
+echo -e "${RED} ==================DOOOOONNNNNNEEEEE=================\n"
+echo -e "${RED} ==================DOOOOONNNNNNEEEEE=================\n"
+echo -e "${RED} ==================DOOOOONNNNNNEEEEE=================\n"
+echo -e "${RED} ==================DOOOOONNNNNNEEEEE=================\n"
+
+
